e735a67551e3202bd310543edbab860a83b3b3a1
Tim Quimbo <tquimbo1@gmail.com>
2021-11-21 00:06:53 -0500

    commit tree b166c03e15833553fa4d139160d05fb263ce2444

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your functions here
+
+
+function map(array, callback) {
+    let x = [];
+    for (let i = 0; i < array.length; i++ ) {
+      let a = array[i];
+      x.push(callback(a));
+    }
+    return x;
+  }
+  
+  function reduce(array, callback, startingPoint) {
+    let x = !!startingPoint ? startingPoint : array[0];
+    let i = !!startingPoint ? 0 : 1;
+    for(; i < array.length; i++){
+      x = callback(array[i], x);
+    }
+    return x;
+  }
